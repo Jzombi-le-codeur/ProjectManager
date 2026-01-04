a07fc3096b800dcdc5c36c8512d43e54b0d33e06
Jzombi <jinot2t@gmail.com>
2026-01-04 12:10:00 +0100

    Ajout d'un FrontEnd, qui gère actuellement l'affichage des tâches pour un projet

---
+++ FrontEnd/front_end/src/App/App.jsx
@@ -0,0 +1,5 @@
+import Project from './Project/Project'
+
+export default function App() {
+    return <Project />
+}
---
+++ FrontEnd/front_end/src/Components/Task/Task/Task.jsx
@@ -0,0 +1,13 @@
+export default function Task() {
+    return (
+        <div className="task">
+            <div className="task-tag">
+                <p className="tag tag-start">Tag</p>
+                <p className="tag tag-end">Tag 2</p>
+            </div>
+            <div className="task-description">
+                <p>Faire la tâche immédiatement grossss</p>
+            </div>
+        </div>
+    )
+};
